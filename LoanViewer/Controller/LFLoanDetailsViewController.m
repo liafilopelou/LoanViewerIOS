10aae0aad4f5c5f5b991a82cb33a5aaf4d86cf3d
Lia Filopelou <myliamail.dev@gmail.com>
2015-11-16 01:47:53 +0100

    Map implementation

--- LoanViewer/Controller/LFLoanDetailsViewController.m
+++ LoanViewer/Controller/LFLoanDetailsViewController.m
@@ -5,12 +5,16 @@
 #import "Geo+CoreDataProperties.h"
 #import <MapKit/MapKit.h>
 
-@interface LFLoanDetailsViewController ()
+static NSString *const PinAnnotationIdentifier = @"PinAnnotationView";
+
+@interface LFLoanDetailsViewController () <MKMapViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UILabel *statusLabel;
 @property (weak, nonatomic) IBOutlet UILabel *activityLabel;
 @property (weak, nonatomic) IBOutlet UILabel *sectorLabel;
 @property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (assign, nonatomic) CLLocationCoordinate2D location2d;
 
 @end
 
@@ -36,8 +40,39 @@
     NSArray *coordinates = [pairs componentsSeparatedByString:@" "];
     NSString *latitude = coordinates[0];
     NSString *longtitude = coordinates[1];
-    CLLocationCoordinate2D location2d = CLLocationCoordinate2DMake([latitude doubleValue] , [longtitude doubleValue]);
+    self.location2d = CLLocationCoordinate2DMake([latitude doubleValue] , [longtitude doubleValue]);
     
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(self.location2d, 80000, 80000);
+    [self.mapView setRegion:region animated:true];
+    [self.mapView setDelegate:self];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+    annotation.coordinate = self.location2d;
+    [self.mapView addAnnotation:annotation];
+}
+
+- (nullable MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
+{
+    if ([annotation isKindOfClass:[MKPointAnnotation class]])
+    {
+        MKPinAnnotationView *annotationView = (MKPinAnnotationView *) [mapView dequeueReusableAnnotationViewWithIdentifier:PinAnnotationIdentifier];
+        if (!annotationView)
+        {
+            annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:PinAnnotationIdentifier];
+            annotationView.animatesDrop = YES;
+        }
+        else
+        {
+            annotationView.annotation = annotation;
+        }
+        return annotationView;
+    }
+    return nil;
 }
 
 @end
